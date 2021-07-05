1798b71be52f075a764234f8654dff28fafa0029
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2021-07-04 20:12:35 -0400

    added mpc, basic parsing function works

--- prompt.c
+++ prompt.c
@@ -1,14 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-// For compiling on Windows
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
-#include<string.h>
 
 static char buffer[2048];
-
-// Fake readline function
 
 char* readline(char* prompt) {
     fputs(prompt, stdout);
@@ -19,14 +15,30 @@ char* readline(char* prompt) {
     return cpy;
 }
 
-// Never used, included by default in windows prompt
 void add_history(char* unused) {}
 
 #else
-#include<editline.h>
+#include <editline.h>
 #endif
 
 int main(int argc, char** argv) {
+    
+    // Parser init
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lizp      = mpc_new("lizp");
+
+    // Definitions
+    mpca_lang(MPCA_LANG_DEFAULT,
+      "                                                          \
+          number      : /-?[0-9]+/ ;                             \
+          operator    : '+' | '-' | '*' | '/' ;                  \ 
+          expr        : <number> | '(' <operator> <expr>+ ')' ;  \ 
+          lizp        : /^/ <operator> <expr>+ /$/ ;             \ 
+      ",
+      Number, Operator, Expr, Lizp);
+
 
     puts("Lizp Version -1");
     puts("Press Ctrl+c exit");
@@ -38,10 +50,19 @@ int main(int argc, char** argv) {
 
         add_history(input);
 
-        printf("No you're a %s", input);
+        mpc_result_t r;
+        if(mpc_parse("<stdin>", input, Lizp, &r)) {
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
     }
+    
+    mpc_cleanup(4, Number, Operator, Expr, Lizp);
 
     return 0;
 }
