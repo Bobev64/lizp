8b2c06e2e2dbd920325c03eb27a1461660043bac
Bobev64 <68569762+Bobev64@users.noreply.github.com>
2021-07-06 17:30:40 -0400

    prompt now evaluates

--- prompt.c
+++ prompt.c
@@ -21,6 +21,10 @@ void add_history(char* unused) {}
 #include <editline.h>
 #endif
 
+int number_of_nodes(mpc_ast_t* t);
+long eval(mpc_ast_t* t);
+long eval_op(long x, char* op, long y);
+
 int main(int argc, char** argv) {
     
     // Parser init
@@ -38,10 +42,9 @@ int main(int argc, char** argv) {
           lizp        : /^/ <operator> <expr>+ /$/ ;             \
       ",
       Number, Operator, Expr, Lizp);
-
-
+   
     puts("Lizp Version -1");
-    puts("Press Ctrl+c exit");
+    puts("Press Ctrl+c to exit");
 
     // C doesn't support bools in stdio.h so 1 suffices for true
     while(1) {
@@ -51,18 +54,65 @@ int main(int argc, char** argv) {
         add_history(input);
 
         mpc_result_t r;
+
         if(mpc_parse("<stdin>", input, Lizp, &r)) {
-            mpc_ast_print(r.output);
+
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
+
         } else {
             mpc_err_print(r.error);
             mpc_err_delete(r.error);
         }
-
         free(input);
     }
     
     mpc_cleanup(4, Number, Operator, Expr, Lizp);
+
+    return 0;
+}
+
+int number_of_nodes(mpc_ast_t* t) {
+    if (t->children_num == 0) { return 1; }
+    if (t->children_num >= 1) {
+        int total = 1;
+        for (int i = 0; i < t->children_num; i++) {
+            total = total + number_of_nodes(t->children[i]);
+        }
+        return total;
+    }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    //returns number if it's tagged as such
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // Operator is always second child
+    char* op = t->children[1]->contents;
+    
+    // Store third value
+    long x = eval(t->children[2]);
+
+    // Iterate remaining values and combine
+    int i = 3;
+        while (strstr(t->children[i]->tag, "expr")) {
+            x = eval_op(x, op, eval(t->children[i]));
+            i++;
+        }
+    return x;
+}
+
+long eval_op(long x, char* op, long y) {
+
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
 
     return 0;
 }
